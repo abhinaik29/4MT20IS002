e4d62e0ad7abd8d75ed4bdf17da0329ecfd5b2e8
abhinaik29 <96305646+abhinaik29@users.noreply.github.com>
2023-10-02 17:21:33 +0530

    Create prog3.py

---
+++ Assignment_7/prog3.py
@@ -0,0 +1,11 @@
+
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+
+result_dict = {}
+result_dict.update(dic1)
+result_dict.update(dic2)
+result_dict.update(dic3)
+
+print(result_dict)
